170c4913b3299a802a3db555cb440b7bd162b7d9
Joao-Pedro-Duo <joaopedroduo09@gmail.com>
2026-08-18 08:59:16 -0300

    💄 UI:  Criação das Rotas com o Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Footer from "./components/footer/Footer"
 import Navbar from "./components/navbar/Navbar"
 import Home from "./pages/home/Home"
+import Cadastro from './pages/cadastro/Cadastro'
+import Login from './pages/login/Login'
 
 
 
@@ -8,9 +11,17 @@ function App() {
 
   return (
     <>
+    <BrowserRouter>
       <Navbar />
-      <Home />
+      <div className='min-h-[80vh]'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      </div>
       <Footer />
+    </BrowserRouter>
     </>
   )
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 
 
 function Navbar() {
@@ -6,14 +7,14 @@ function Navbar() {
             <div className="w-full flex justify-center py-4 bg-indigo-900 text-white">
 
                 <div className=" container flex justify-between text-lg mx-8">
-                    Blog Pessoal
+                    <Link to="/" className="text-2-xl font-bold">Blog Pessoal</Link>
 
                     <div className="flex gap-4">
                         Postagens
                         Temas
                         Cadastrar Tema
                         Perfil 
-                        Sair
+                        <Link to="/login" className="hover:underline">Sair</Link>
                     </div>
 
                 </div>
